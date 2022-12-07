1aa598a3f4c1cd85077fc74c8c05e47e4f9f1c91
Yamuna Paudel <66802544+Paudel514@users.noreply.github.com>
2022-12-06 20:27:52 -0500

    Add files via upload

--- Assignment_8.py
+++ Assignment_8.py
@@ -20,13 +20,13 @@ R    = 0.08   # Radius of ball in meter
 rho  = 1.22 # density kg/m^3
 
 # Define function for calculating the values of the differential equations
-def f(r, constants_product):
+def f(r, constants):
   x = r[0]
   y = r[1]
   Vx = r[2]
   Vy = r[3]
-  fxv = (-1) * constants_product * Vx * np.sqrt(Vx**2+Vy**2)
-  fyv = (-1*g) - constants_product * Vy * np.sqrt(Vx**2+Vy**2)
+  fxv = (-1) * constants* Vx * np.sqrt(Vx**2+Vy**2)
+  fyv = (-1*g) - constants* Vy * np.sqrt(Vx**2+Vy**2)
   return np.array([Vx, Vy, fxv, fyv])
 
 # Create method to calculate the trajectory of an object of given mass in kg
@@ -35,14 +35,14 @@ def cb_trajectory(mass):
   t_points = np.arange(t_initial, t_final + h, h)
   r_points = np.zeros((len(t_points), 4))
   r_points[0,:] = [x_i, y_i, vx_i, vy_i]
-  constants_product = (np.pi * R**2 * rho * C) / (2.0 * mass)
+  constants = (np.pi * R**2 * rho * C) / (2.0 * mass)
 
   #Runge-Kutta method 
   for i in range(1, len(t_points)):
-    k1 = f(r_points[i-1,:], constants_product)
-    k2 = f(r_points[i-1,:] + (h/2)*k1, constants_product)
-    k3 = f(r_points[i-1,:] + (h/2)*k2, constants_product)
-    k4 = f(r_points[i-1,:] + h*k3, constants_product)
+    k1 = f(r_points[i-1,:], constants)
+    k2 = f(r_points[i-1,:] + (h/2)*k1, constants)
+    k3 = f(r_points[i-1,:] + (h/2)*k2, constants)
+    k4 = f(r_points[i-1,:] + h*k3, constants)
     if (r_points[i-1,:][0] >= 0) and (r_points[i-1,:][1] >= 0):
       r_points[i, :] = r_points[i-1,:] + (h/6)*(k1 + 2*k2 + 2*k3 + k4)
     else:
@@ -53,7 +53,8 @@ result = cb_trajectory(1) # initial mass in kg
 # Plot the y values as a function of x
 fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(5, 6))
 ax1.plot(result[:, 0], result[:, 1], 'blue',linewidth=1.5)
-ax1.set(ylabel='y(m)')
+ax1.set(xlabel='x(m)',ylabel='y(m)')
+ax1.label_outer()
 ax1.set_title('Cannonball trajectory with air resistance')
 mass_one = cb_trajectory(1)
 mass_two  = cb_trajectory(2.5)
